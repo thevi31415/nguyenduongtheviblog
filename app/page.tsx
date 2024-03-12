8acd6cdaf0720bd0dd4905ca878a0ef5585158ff
vi nguyen <nguyenduongthevi31415@gmail.com>
2024-03-12 10:14:46 +0700

    cập nhật favicon

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1>
-        Xin chào đây là blog các nhân của mình ! Đang trong quá trình xây dựng
+        Xin chào đây là blog cá nhân của mình ! Đang trong quá trình xây dựng
         nhé
         <br />
         3/12/2024 !
