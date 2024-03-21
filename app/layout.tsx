f8e72f093e39f7113b9677c39b2eb5c1946504bc
vi nguyen <nguyenduongthevi31415@gmail.com>
2024-03-21 19:45:15 +0700

    Cập nhật footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -133,16 +133,27 @@ export default function RootLayout({
   const footer = (
     <>
       <div className="text-center">
+        <p className="text-sm text-center text-gray-500 dark:text-gray-400 mb-1">
+          Made by{" "}
+          <a
+            href="https://github.com/thevi31415"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-medium hover:text-gray-800 dark:hover:text-gray-200 cursor-pointer"
+          >
+            @thevi31415
+          </a>
+        </p>
         <span className="block text-sm text-center text-gray-500 dark:text-gray-400 mb-3">
           © 2024 Nguyen Duong The Vi. All Rights Reserved.
         </span>
-        <a
+        {/* <a
           href="https://github.com/thevi31415/nguyenduongtheviblog"
           target="_blank"
           className="  text-lg rounded-full bg-sky-400/10 px-2 py-1 font-medium leading-5 text-sky-600 dark:text-sky-400"
         >
           Source code
-        </a>
+        </a> */}
         <ul className="flex justify-center mt-5 space-x-5">
           <li>
             <a
--- app/project/page.tsx
+++ app/project/page.tsx
@@ -6,8 +6,8 @@ export const metadata: Metadata = {
 };
 export default function Project() {
   return (
-    <div>
-      <h1>Project</h1>
-    </div>
+    <ul className="grid max-w-[26rem] sm:max-w-[52.5rem] mt-16 sm:mt-20 md:mt-32 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mx-auto gap-6 lg:gap-y-8 xl:gap-x-8 lg:max-w-7xl px-4 sm:px-6 lg:px-8">
+      Project
+    </ul>
   );
 }
