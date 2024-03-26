39da5e4ee126ac6c46c5c7d123d7415539bf2122
vi nguyen <nguyenduongthevi31415@gmail.com>
2024-03-26 07:11:31 +0700

    Cập nhật giao diện trang Quiz

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import Image from "next/image";
 import Link from "next/link";
 import fs from "fs";
@@ -67,7 +68,27 @@ const getTagsMetadata = (): TagsMetadata[] => {
     return dateB.getTime() - dateA.getTime();
   });
 };
+const formatDate = (dateString: String) => {
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
 
+  const [day, month, year] = dateString.split("-").map(Number);
+  const monthName = months[month - 1];
+
+  return `${day} ${monthName}, ${year}`;
+};
 export default function Blog(props: any) {
   const quizMetadata = getQuizMetadata();
   const tagsMetadata = getTagsMetadata();
@@ -83,7 +104,7 @@ export default function Blog(props: any) {
   const postPreviews = quizMetadata.slice(start, end).map((quiz, index) => (
     // eslint-disable-next-line react/jsx-key
     <div key={quiz.IDT}>
-      <div className="flex flex-col w-full border hover:bg-slate-50 rounded p-4 mb-1 hover:shadow-sm">
+      {/* <div className="flex flex-col w-full border hover:bg-slate-50 rounded p-4 mb-1 hover:shadow-sm">
         <span className="font-bold">
           ❓
           <Link href={`/quiz/${quiz.IDT}`} className="gradient-text">
@@ -94,6 +115,62 @@ export default function Blog(props: any) {
         <div className="mt-auto">
           <time className="text-sm text-slate-400">🕖{quiz.date}</time>
         </div>
+      </div> */}
+
+      <div className="ps-2 my-2 first:mt-0">
+        <h3 className="text-xs font-medium uppercase text-gray-500 dark:text-gray-400">
+          {formatDate(quiz.date)}
+        </h3>
+      </div>
+      <div className="flex gap-x-3 relative group rounded-lg hover:bg-gray-100 dark:hover:bg-white/10">
+        <Link
+          href={`/quiz/${quiz.IDT}`}
+          className="absolute inset-0 z-[1]"
+        ></Link>
+        <div className="relative last:after:hidden after:absolute after:top-0 after:bottom-0 after:start-3.5 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 dark:after:bg-gray-700 dark:group-hover:after:bg-gray-600">
+          <div className="relative z-10 size-7 flex justify-center items-center">
+            <div className="size-2 rounded-full bg-white border-2 border-gray-300 group-hover:border-gray-600 dark:bg-gray-800 dark:border-gray-600"></div>
+          </div>
+        </div>
+        <div className="grow p-2 pb-8">
+          <h3 className="flex gap-x-1.5 font-semibold text-gray-800 dark:text-white">
+            <svg
+              className="flex-shrink-0 size-4 mt-1"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z" />
+              <polyline points="14 2 14 8 20 8" />
+              <line x1="16" x2="8" y1="13" y2="13" />
+              <line x1="16" x2="8" y1="17" y2="17" />
+              <line x1="10" x2="8" y1="9" y2="9" />
+            </svg>
+            {quiz.title}
+          </h3>
+          <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
+            {quiz.subtitle}
+          </p>
+          <button
+            type="button"
+            className="mt-1 -ms-1 p-1 relative z-10 inline-flex items-center gap-x-2 text-xs rounded-lg border border-transparent text-gray-500 hover:bg-white hover:shadow-sm disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+          >
+            <Image
+              width={500}
+              height={500}
+              className="flex-shrink-0 size-4 rounded-full"
+              src="/profile.jpg"
+              alt="Image Description"
+            />
+            Nguyen Duong The Vi
+          </button>
+        </div>
       </div>
     </div>
   ));
@@ -101,12 +178,14 @@ export default function Blog(props: any) {
   return (
     <div>
       <div className="overflow-hidden">
+        {/* 
+        <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 mb-2">
+          {postPreviews}
+        </div>{" "} */}
         <h4 className="  text-2xl font-medium text-gray-700 mb-5" id="new">
           ✍🏻 Quiz
         </h4>
-        <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 mb-2">
-          {postPreviews}
-        </div>{" "}
+        <div className="timeline mb-3">{postPreviews}</div>
       </div>
     </div>
   );
